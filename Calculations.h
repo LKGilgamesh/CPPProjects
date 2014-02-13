2a441587bc0a77404f26b6e81c8e4d865127a873
LKGilgamesh <leoncio.kian@gmail.com>
2014-02-13 12:30:05 -0500

    Create Calculations.h

---
+++ Calculations.h
@@ -0,0 +1,14 @@
+/*
+ * Calculations.h
+ *
+ *  Created on: Jan 30, 2014
+ *      Author: lkian
+ */
+
+#ifndef CALCULATIONS_H_
+#define CALCULATIONS_H_
+
+int calculate(char Op,double a,double b );
+int exceptions(double a);
+
+#endif /* CALCULATIONS_H_ */
