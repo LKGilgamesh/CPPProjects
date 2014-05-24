7bfc373e9112f6d4387ecff169b157382c57b273
Leoncio Kian <leoncio.kian@gmail.com>
2014-05-24 10:10:03 -0400

    first commit ipad

---
+++ COP round2/Project1.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(){
+	ifstream input;
+	ofstream output;
+	
+	int select=20;
+	std:: cout << "0. exit\n 1. input file\n2. union file\n3. subtract file\n4. difference file\n" <<
+				  "5. intersect file\n6. reset to empty set\n7. output file\n8. print set\n 9. find\n" <<
+				  "10. insert\n11. delete\n12. verbose output\n13. normal output\n14. silent output\n 15. help\n" << endl;
+	std::cin >> select;
+	if(select == 0){
+		
+	}
+	else if(select == 1){
+		
+	}
+	else if(select == 2){
+		
+	}
+	else if(select == 3){
+		
+	}
+	else if(select == 4){
+		
+	}
+	else if(select == 5){
+		
+	}
+	else if(select == 6){
+		
+	}
+	else if(select == 7){
+		
+	}
+	else if(select == 8){
+		
+	}
+	else if(select == 9){
+		
+	}
+	else if(select == 10){
+		
+	}
+	else if(select == 11){
+		
+	}
+	else if(select == 12){
+		
+	}
+	else if(select == 13){
+		
+	}
+	else if(select == 14){
+		
+	}
+	else if(select == 15){
+		
+	}
+	return 0;
+}
